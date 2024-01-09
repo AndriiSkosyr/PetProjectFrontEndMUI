7ee8f0e88c8d28aa43644d41b57f61b6a58e19b2
Andrii <andrewskosyr@gmail.com>
2024-01-09 22:24:54 +0200

    MainPage v.0.3 Started working on home component. Added grid layout for home component.

--- summarizer-app/src/Components/Home.jsx
+++ summarizer-app/src/Components/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Box, Grid } from "@mui/material";
 
 const Home = () => {
-    return <div>
-        <h1>Home</h1>
-    </div>
+    return (
+        <Grid container>
+            <Grid xs={3}>
+                <Grid container rowSpacing={2}>
+                    <Grid xs={12}><Box><h1>box1</h1></Box></Grid>
+                    <Grid xs={12}><h1>list2</h1></Grid>
+                    <Grid xs={12}><h1>list3</h1></Grid>
+                </Grid>
+            </Grid>
+            <Grid xs={9}>
+                <Grid container rowSpacing={2}> 
+                    <Grid xs={12}><h1>calendar</h1></Grid>
+                    <Grid xs={12}><h1>controls</h1></Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default Home;
